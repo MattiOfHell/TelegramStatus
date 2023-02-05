cad10a62a1071f53e392cf043e23f176aee8ac86
MattiOfHell <75907402+MattiOfHell@users.noreply.github.com>
2023-02-05 15:43:05 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+pip install telethon
+echo '-------------------------'
+echo '-------------------------'
+echo '-------------------------'
+echo Installation complete! You can close this file
+echo '-------------------------'
+echo '-------------------------'
+echo '-------------------------'
+
+pause
